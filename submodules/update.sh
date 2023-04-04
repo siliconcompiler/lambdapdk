4996deb2de4a33e0a5173a1c09dfa834739da635
aolofsson <andreas@zeroasic.com>
2023-04-03 22:26:23 -0400

    Update submodules

--- submodules/update.sh
+++ submodules/update.sh
@@ -6,9 +6,3 @@ git add submodules/
 
 # Step 3: commit changes
 git commit -m "Update submodules" -e
-
-echo ""
-echo "NOTE:"
-echo "Be sure to push any changed submodules before pushing ebrick!"
-echo "Run $ git submodule foreach --recursive \"git push\" to do this automatically."
-
