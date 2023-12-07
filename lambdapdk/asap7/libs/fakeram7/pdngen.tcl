0a87a546e33a9b76e3cd5a55ec46638feca24ef7
Peter Gadfort <gadfort@zeroasic.com>
2023-12-07 03:58:01 -0500

    fix tclint

--- lambdapdk/asap7/libs/fakeram7/pdngen.tcl
+++ lambdapdk/asap7/libs/fakeram7/pdngen.tcl
@@ -1,5 +1,8 @@
 ####################################
 # grid for: fameram7
 ####################################
-define_pdn_grid -name {fameram7} -voltage_domains {CORE} -macro -halo {1.0 1.0 1.0 1.0} -cells {fakeram7_.*}
+define_pdn_grid -name {fameram7} -voltage_domains {CORE} \
+    -macro \
+    -halo {1.0 1.0 1.0 1.0} \
+    -cells {fakeram7_.*}
 add_pdn_connect -grid {fameram7} -layers {M4 M5}
