29e4329b75393e53d750b3931407fb433a8fd353
Peter Gadfort <gadfort@zeroasic.com>
2023-12-12 11:12:44 -0500

    ensure lambda is added for srams

--- lambdapdk/freepdk45/libs/fakeram45.py
+++ lambdapdk/freepdk45/libs/fakeram45.py
@@ -11,14 +11,16 @@ def setup(chip):
 
     for config in ('64x32', '128x32', '256x32', '256x64', '512x32', '512x64'):
         mem_name = f'fakeram45_{config}'
-        lib = Library(chip, mem_name)
+        lib = Library(chip, mem_name, package='lambdapdk')
         path_base = 'lambdapdk/freepdk45/libs/fakeram45'
-        lib.add('output', stackup, 'lef', f'{path_base}/lef/{mem_name}.lef', package='lambdapdk')
-        lib.add('output', corner, 'nldm', f'{path_base}/nldm/{mem_name}.lib', package='lambdapdk')
+        lib.add('output', stackup, 'lef', f'{path_base}/lef/{mem_name}.lef')
+        lib.add('output', corner, 'nldm', f'{path_base}/nldm/{mem_name}.lib')
 
-        lib.set('option', 'file', 'openroad_pdngen', f'{path_base}/pdngen.tcl', package='lambdapdk')
+        lib.set('option', 'file', 'openroad_pdngen', f'{path_base}/pdngen.tcl')
 
         lib.set('option', 'var', 'klayout_allow_missing_cell', mem_name)
+
+        lib.set('option', 'ydir', f'{path_base}/lambda')
 
         libs.append(lib)
 
--- lambdapdk/sky130/libs/sky130sram.py
+++ lambdapdk/sky130/libs/sky130sram.py
@@ -10,20 +10,19 @@ def setup(chip):
 
     for config in ('64x256',):
         mem_name = f'sky130_sram_1rw1r_{config}_8'
-        lib = Library(chip, mem_name)
+        lib = Library(chip, mem_name, package='lambdapdk')
         path_base = 'lambdapdk/sky130/libs/sky130sram'
-        lib.add('output', stackup, 'lef', f'{path_base}/{mem_name}/lef/{mem_name}.lef.gz',
-                package='lambdapdk')
-        lib.add('output', stackup, 'gds', f'{path_base}/{mem_name}/gds/{mem_name}.gds',
-                package='lambdapdk')
-        lib.add('output', stackup, 'cdl', f'{path_base}/{mem_name}/spice/{mem_name}.lvs.sp',
-                package='lambdapdk')
+        lib.add('output', stackup, 'lef', f'{path_base}/{mem_name}/lef/{mem_name}.lef.gz')
+        lib.add('output', stackup, 'gds', f'{path_base}/{mem_name}/gds/{mem_name}.gds')
+        lib.add('output', stackup, 'cdl', f'{path_base}/{mem_name}/spice/{mem_name}.lvs.sp')
 
         for corner in ('slow', 'fast', 'typical'):
             lib.add('output', corner, 'nldm',
-                    f'{path_base}/{mem_name}/nldm/{mem_name}_TT_1p8V_25C.lib', package='lambdapdk')
+                    f'{path_base}/{mem_name}/nldm/{mem_name}_TT_1p8V_25C.lib')
 
-        lib.set('option', 'file', 'openroad_pdngen', f'{path_base}/pdngen.tcl', package='lambdapdk')
+        lib.set('option', 'file', 'openroad_pdngen', f'{path_base}/pdngen.tcl')
+
+        lib.set('option', 'ydir', f'{path_base}/lambda')
 
         libs.append(lib)
 
