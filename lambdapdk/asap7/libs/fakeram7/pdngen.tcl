63342ea43c4317d61d084f5ce9fbadbbd3b1a1d8
Peter Gadfort <gadfort@zeroasic.com>
2024-02-08 19:18:50 +0000

    update asap7 memory pdn

--- lambdapdk/asap7/libs/fakeram7/pdngen.tcl
+++ lambdapdk/asap7/libs/fakeram7/pdngen.tcl
@@ -5,4 +5,6 @@ define_pdn_grid -name {fameram7} -voltage_domains {CORE} \
     -macro \
     -halo {1.0 1.0 1.0 1.0} \
     -cells {fakeram7_.*}
+add_pdn_stripe -grid {fameram7} -layer M5 -width 0.12 -pitch 0.6
 add_pdn_connect -grid {fameram7} -layers {M4 M5}
+add_pdn_connect -grid {fameram7} -layers {M5 M6}
