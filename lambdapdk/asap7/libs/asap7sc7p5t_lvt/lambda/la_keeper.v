dd7a91f4a927eea255ce8a993147ec5e4aed9232
Peter Gadfort <gadfort@zeroasic.com>
2024-02-15 09:46:35 -0500

    update lambdalib

---
+++ lambdapdk/asap7/libs/asap7sc7p5t_lvt/lambda/la_keeper.v
@@ -0,0 +1,11 @@
+//#############################################################################
+//# Function: Charge Keeper Cell                                              #
+//# Copyright: Lambda Project Authors. All rights Reserved.                   #
+//# License:  MIT (see LICENSE file in Lambda repository)                     #
+//#############################################################################
+
+module la_keeper #(parameter PROP = "DEFAULT")   (
+    inout z
+    );
+
+endmodule
--- scripts/generate_lamdbalib.py
+++ scripts/generate_lamdbalib.py
@@ -4,12 +4,8 @@
 # Script for auto-generating lambda views using Yosys
 ###############################################################################
 
-import git
-import glob
 import os
-import shutil
-import tempfile
-import siliconcompiler
+import lambdalib
 
 from siliconcompiler.targets import (
     skywater130_demo,
@@ -19,24 +15,8 @@ from siliconcompiler.targets import (
 )
 
 if __name__ == "__main__":
-    # Clone clean copy of lambdalib
-    git_path = 'https://github.com/siliconcompiler/lambdalib.git'
-    repo_dir = tempfile.TemporaryDirectory(prefix='lambdalib_')
-    repo_work_dir = repo_dir.name
-    git.Repo.clone_from(git_path, repo_work_dir)
+    pdk_root = os.path.dirname(os.path.dirname(__file__))
 
-    pdk_root = os.path.join(os.path.dirname(__file__), '..')
-    # Get List of stdlib cells in lambdalib
-    cells_dir = os.path.join(repo_work_dir, 'stdlib', 'rtl')
-    cells = glob.glob(f'{cells_dir}/la_*.v')
-
-    exclude = ['la_decap',
-               'la_keeper',
-               'la_footer',
-               'la_header',
-               'la_antenna']
-
-    # TODO: make script less janky
     pdks = {
         "sky130": {
             "target": skywater130_demo,
@@ -71,29 +51,4 @@ if __name__ == "__main__":
         target = info["target"]
 
         for lib in info['libs']:
-            lambda_dir = f"{pdk_root}/lambdapdk/{pdk}/libs/{lib}/lambda"
-            shutil.rmtree(lambda_dir, ignore_errors=True)
-            os.makedirs(lambda_dir, exist_ok=True)
-
-            for verilog in cells:
-                cell_name, _ = os.path.splitext(os.path.basename(verilog))
-
-                if cell_name in exclude:
-                    continue
-
-                jobname = f"{target.__name__}-{lib}"
-
-                chip = siliconcompiler.Chip(cell_name)
-                chip.input(verilog)
-                chip.load_target(target)
-                chip.set('asic', 'logiclib', lib)
-                chip.set('option', 'to', 'syn')
-                chip.set('option', 'quiet', True)
-                chip.set('option', 'resume', True)
-                chip.set('option', 'jobname', jobname)
-
-                chip.add('option', 'ydir', cells_dir)
-                chip.run()
-
-                shutil.copy(chip.find_result("vg", step="syn", index=0),
-                            lambda_dir)
+            lambdalib.generate(target, lib, f"{pdk_root}/lambdapdk/{pdk}/libs/{lib}/lambda")
