825846b2fa2ae8f95ccacf4ab383948722e72f30
Peter Gadfort <gadfort@zeroasic.com>
2024-02-20 10:35:56 -0500

    move to use sc_package.register_python_data_source

--- lambdapdk/__init__.py
+++ lambdapdk/__init__.py
@@ -1,18 +1,15 @@
-import os
+import siliconcompiler.package as sc_package
+
+
 __version__ = "0.1.8"
 
 
 def register_data_source(chip):
-    # check if local
-    root_path = os.path.dirname(os.path.dirname(__file__))
-    test_path = os.path.join(root_path, 'lambdapdk', 'asap7', 'base', 'apr', 'asap7_tech.lef')
-    if os.path.exists(test_path):
-        path = root_path
-        ref = None
-    else:
-        path = 'git+https://github.com/siliconcompiler/lambdapdk.git'
-        ref = f'v{__version__}'
-
-    chip.register_package_source(name='lambdapdk',
-                                 path=path,
-                                 ref=ref)
+    sc_package.register_python_data_source(
+        chip,
+        "lambdapdk",
+        "lambdapdk",
+        "git+https://github.com/siliconcompiler/lambdapdk.git",
+        alternative_ref=f"v{__version__}",
+        python_module_path_append=".."
+    )
