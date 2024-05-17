6fe70b0bbc368370eda737cd9059b908155e0b21
Peter Gadfort <peter.gadfort@gmail.com>
2024-05-17 16:58:54 -0400

    fix tclint

--- lambdapdk/sky130/libs/sky130hd/apr/openroad/pdngen.tcl
+++ lambdapdk/sky130/libs/sky130hd/apr/openroad/pdngen.tcl
@@ -15,7 +15,9 @@ set met5_pitch [expr {([lindex [ord::get_core_area] 3] - [lindex [ord::get_core_
 if {$met5_pitch > 27.200} {
     set met5_pitch 27.200
 }
-add_pdn_stripe -grid {grid} -layer {met4} -width {1.600} -pitch $met4_pitch -offset [expr {$met4_pitch / 4}]
-add_pdn_stripe -grid {grid} -layer {met5} -width {1.600} -pitch $met5_pitch -offset [expr {$met5_pitch / 4}]
+add_pdn_stripe -grid {grid} -layer {met4} -width {1.600} -pitch $met4_pitch \
+    -offset [expr {$met4_pitch / 4}]
+add_pdn_stripe -grid {grid} -layer {met5} -width {1.600} -pitch $met5_pitch \
+    -offset [expr {$met5_pitch / 4}]
 add_pdn_connect -grid {grid} -layers {met1 met4}
 add_pdn_connect -grid {grid} -layers {met4 met5}
