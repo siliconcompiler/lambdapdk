714e9e366ad980b2131015bc19f70d966ac9ea82
Peter Gadfort <gadfort@zeroasic.com>
2024-08-29 08:53:04 -0400

    fix gf180 io

--- lambdapdk/gf180/libs/gf180mcu_fd_io/lambda/la_iocut.v
+++ lambdapdk/gf180/libs/gf180mcu_fd_io/lambda/la_iocut.v
@@ -3,15 +3,22 @@ module la_iocut #(
     parameter SIDE  = "NO",       // "NO", "SO", "EA", "WE"
     parameter RINGW = 8           // width of io ring
 ) (
-    inout vdd,  // core supply
-    inout vss,  // core ground
-    inout vddio,  // io supply
-    inout vssio,  // io ground
-    inout [RINGW-1:0] ioring  // generic io-ring interface
+    // ground never cut
+    inout             vss,
+    // left side (viewed from center)
+    inout             vdd0,     // core
+    inout             vddio0,   // io supply
+    inout             vssio0,   // left io ground
+    inout [RINGW-1:0] ioring0,  // left ioring
+    // right side (viewed from center)
+    inout             vdd1,     // core (from center)
+    inout             vddio1,   // io supply
+    inout             vssio1,   // left io ground
+    inout [RINGW-1:0] ioring1   // left ioring
 );
 
 
-  //TODO: selecct cut cell based on type
+  //TODO: select cut cell based on type
   gf180mcu_fd_io__brk2 iocut (.VSS(vss));
 
 endmodule
