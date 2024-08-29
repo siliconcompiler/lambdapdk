41757675405cfc93d25b53f63218649e37322f94
Peter Gadfort <gadfort@zeroasic.com>
2024-08-29 08:51:50 -0400

    fix IO library for sky130

--- lambdapdk/sky130/libs/sky130io/lambda/la_iopoc.v
+++ lambdapdk/sky130/libs/sky130io/lambda/la_iopoc.v
@@ -1,6 +1,7 @@
 module la_iopoc #(
     parameter PROP  = "DEFAULT",  // cell type
     parameter SIDE  = "NO",       // "NO", "SO", "EA", "WE"
+    parameter CFGW  = 16,         // width of core config bus
     parameter RINGW = 8           // width of io ring
 ) (
     inout             vdd,    // core supply
@@ -8,6 +9,7 @@ module la_iopoc #(
     inout             vddio,  // io supply
     inout             vssio,  // io ground
     inout [RINGW-1:0] ioring  // generic io-ring interface
+    input [ CFGW-1:0] cfg     // generic config interface
 );
 
 endmodule
