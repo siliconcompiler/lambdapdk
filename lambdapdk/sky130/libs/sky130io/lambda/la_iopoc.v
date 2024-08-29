88110e1e840e724c9f6899af2c0e07037bc46840
Peter Gadfort <gadfort@zeroasic.com>
2024-08-29 09:18:03 -0400

    fix verilog syntax error

--- lambdapdk/sky130/libs/sky130io/lambda/la_iopoc.v
+++ lambdapdk/sky130/libs/sky130io/lambda/la_iopoc.v
@@ -4,12 +4,12 @@ module la_iopoc #(
     parameter CFGW  = 16,         // width of core config bus
     parameter RINGW = 8           // width of io ring
 ) (
-    inout             vdd,    // core supply
-    inout             vss,    // core ground
-    inout             vddio,  // io supply
-    inout             vssio,  // io ground
-    inout [RINGW-1:0] ioring  // generic io-ring interface
-    input [ CFGW-1:0] cfg     // generic config interface
+    inout             vdd,     // core supply
+    inout             vss,     // core ground
+    inout             vddio,   // io supply
+    inout             vssio,   // io ground
+    inout [RINGW-1:0] ioring,  // generic io-ring interface
+    input [ CFGW-1:0] cfg      // generic config interface
 );
 
 endmodule
