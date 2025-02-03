1cffcba4d303d5afc056d30b0fe379a71aa2bc6b
Peter Gadfort <peter.gadfort@gmail.com>
2025-02-03 16:16:14 -0700

    fix ihp130 power grid to account for poor sram grid

--- lambdapdk/ihp130/libs/sg13g2_sram/apr/openroad/pdngen.tcl
+++ lambdapdk/ihp130/libs/sg13g2_sram/apr/openroad/pdngen.tcl
@@ -1,10 +1,19 @@
 ####################################
 # grid for: sky130sram
 ####################################
-define_pdn_grid -name {sg13g2_sram} -voltage_domains {CORE} -macro \
-    -orient {R0 R180 MX MY} \
-    -halo {2.0 2.0 2.0 2.0} \
+define_pdn_grid -name {sg13g2_sram_R0} -voltage_domains {CORE} -macro \
+    -orient {R0 MX MY} \
+    -halo {1.0 1.0 1.0 1.0} \
     -cells {RM_IHPSG13_1P_.*}
-add_pdn_stripe -grid {sg13g2_sram} -layer {Metal5} -width {2.200} -pitch {22.0} -offset {11.0}
-add_pdn_connect -grid {sg13g2_sram} -layers {Metal4 Metal5}
-add_pdn_connect -grid {sg13g2_sram} -layers {Metal5 TopMetal1}
+add_pdn_stripe -grid {sg13g2_sram_R0} -layer {TopMetal1} -width {2.000} -pitch {20.0} -offset {1.0}
+add_pdn_connect -grid {sg13g2_sram_R0} -layers {Metal4 TopMetal1}
+add_pdn_connect -grid {sg13g2_sram_R0} -layers {TopMetal1 TopMetal2}
+
+define_pdn_grid -name {sg13g2_sram_R180} -voltage_domains {CORE} -macro \
+    -orient {R180} \
+    -halo {1.0 1.0 1.0 1.0} \
+    -cells {RM_IHPSG13_1P_.*}
+add_pdn_stripe -grid {sg13g2_sram_R180} -layer {Metal5} -width {2.200} -pitch {22.0} -offset {11.0}
+add_pdn_connect -grid {sg13g2_sram_R180} -layers {Metal4 Metal5}
+add_pdn_connect -grid {sg13g2_sram_R180} -layers {Metal5 TopMetal1}
+add_pdn_connect -grid {sg13g2_sram_R180} -layers {TopMetal1 TopMetal2}
--- lambdapdk/ihp130/libs/sg13g2_stdcell/apr/openroad/pdngen.tcl
+++ lambdapdk/ihp130/libs/sg13g2_stdcell/apr/openroad/pdngen.tcl
@@ -5,7 +5,7 @@ set_voltage_domain -name {CORE} -power {VDD} -ground {VSS}
 #####################################
 # standard cell grid
 ####################################
-define_pdn_grid -name {grid} -voltage_domains {CORE} -pins {TopMetal1}
+define_pdn_grid -name {grid} -voltage_domains {CORE} -pins {TopMetal2}
 add_pdn_stripe -grid {grid} -layer {Metal1} -width {0.44} -followpins
 set met5_pitch [expr {([lindex [ord::get_core_area] 3] - [lindex [ord::get_core_area] 1]) / 2}]
 if {$met5_pitch > 75.6} {
@@ -14,6 +14,10 @@ if {$met5_pitch > 75.6} {
 set top1_pitch [expr {([lindex [ord::get_core_area] 2] - [lindex [ord::get_core_area] 0]) / 2}]
 if {$top1_pitch > 75.6} {
     set top1_pitch 75.6
+}
+set top2_pitch [expr {([lindex [ord::get_core_area] 3] - [lindex [ord::get_core_area] 1]) / 2}]
+if {$top2_pitch > 75.6} {
+    set top2_pitch 75.6
 }
 
 proc snap_grid {value} {
@@ -28,7 +32,10 @@ proc snap_grid {value} {
 
 add_pdn_stripe -grid {grid} -layer {Metal5} -width {2.200} -pitch [snap_grid $met5_pitch] \
     -offset [snap_grid [expr {$met5_pitch / 2}]]
-add_pdn_stripe -grid {grid} -layer {TopMetal1} -width {1.800} -pitch [snap_grid $top1_pitch] \
+add_pdn_stripe -grid {grid} -layer {TopMetal1} -width {2.000} -pitch [snap_grid $top1_pitch] \
     -offset [snap_grid [expr {$top1_pitch / 2}]]
+add_pdn_stripe -grid {grid} -layer {TopMetal2} -width {2.000} -pitch [snap_grid $top2_pitch] \
+    -offset [snap_grid [expr {$top2_pitch / 2}]]
 add_pdn_connect -grid {grid} -layers {Metal1 Metal5}
 add_pdn_connect -grid {grid} -layers {Metal5 TopMetal1}
+add_pdn_connect -grid {grid} -layers {TopMetal1 TopMetal2}
