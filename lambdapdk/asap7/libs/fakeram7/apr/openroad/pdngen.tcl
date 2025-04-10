bd580ef116e08ad666fee5d6ab36e9278a7b4111
Peter Gadfort <peter.gadfort@gmail.com>
2025-04-10 15:56:44 -0400

    fix typo in asap7 grid name

--- lambdapdk/asap7/libs/fakeram7/apr/openroad/pdngen.tcl
+++ lambdapdk/asap7/libs/fakeram7/apr/openroad/pdngen.tcl
@@ -1,10 +1,10 @@
 ####################################
-# grid for: fameram7
+# grid for: fakeram7
 ####################################
-define_pdn_grid -name {fameram7} -voltage_domains {CORE} \
+define_pdn_grid -name {fakeram7} -voltage_domains {CORE} \
     -macro \
     -halo {1.0 1.0 1.0 1.0} \
     -cells {fakeram7_.*}
-add_pdn_stripe -grid {fameram7} -layer M5 -width 0.12 -pitch 0.6
-add_pdn_connect -grid {fameram7} -layers {M4 M5}
-add_pdn_connect -grid {fameram7} -layers {M5 M6}
+add_pdn_stripe -grid {fakeram7} -layer M5 -width 0.12 -pitch 0.6
+add_pdn_connect -grid {fakeram7} -layers {M4 M5}
+add_pdn_connect -grid {fakeram7} -layers {M5 M6}
