ea9ab7ea4a2319c6488c1d7757f1679b68a3e871
Peter Gadfort <peter.gadfort@gmail.com>
2025-04-11 11:27:17 -0400

    unify memory building to single generator and make freepdk45 use new generator

--- lambdapdk/asap7/libs/fakeram7/build.sh
+++ lambdapdk/asap7/libs/fakeram7/build.sh
@@ -9,17 +9,17 @@ work_dir=`mktemp -d`
 
 pushd $work_dir
 
-git clone https://github.com/gadfort/FakeRAM2.0.git
-pushd FakeRAM2.0
-git checkout c0ed59820551c1587472162691e3b81220b35009
-
-python3 run.py $src_path/fakeram7.cfg
-
 mkdir -p $src_path/lef $src_path/nldm $src_path/verilog
 
-cp results/*/*.lef $src_path/lef
-cp results/*/*.lib $src_path/nldm
-cp results/*/*.v $src_path/verilog
+git clone https://github.com/gadfort/FakeRAM2.0.git
+pushd FakeRAM2.0
+git checkout 40a7a4beb7c2018c4c4fe62b0cdd955c0d1150e5
+
+python3 run.py $src_path/fakeram7.cfg --output_dir $work_dir/results
+
+cp $work_dir/results/*/*.lef $src_path/lef
+cp $work_dir/results/*/*.lib $src_path/nldm
+cp $work_dir/results/*/*.v $src_path/verilog
 
 popd
 popd
