181086a21201468779151337b9779feeafda2a89
Peter Gadfort <peter.gadfort@gmail.com>
2025-04-11 11:37:37 -0400

    fix symmetry of freepdk45 memories

--- lambdapdk/freepdk45/libs/fakeram45/build.sh
+++ lambdapdk/freepdk45/libs/fakeram45/build.sh
@@ -13,13 +13,13 @@ mkdir -p $src_path/lef $src_path/nldm $src_path/verilog
 
 git clone https://github.com/gadfort/FakeRAM2.0.git
 pushd FakeRAM2.0
-git checkout 40a7a4beb7c2018c4c4fe62b0cdd955c0d1150e5
+git checkout 454d4dc3b34ddedf9c1edd1da7c29ab14703989e
 
 python3 run.py $src_path/fakeram45.cfg --output_dir $work_dir/results
 
-cp results/*/*.lef $src_path/lef
-cp results/*/*.lib $src_path/nldm
-cp results/*/*.v $src_path/verilog
+cp $work_dir/results/*/*.lef $src_path/lef
+cp $work_dir/results/*/*.lib $src_path/nldm
+cp $work_dir/results/*/*.v $src_path/verilog
 
 popd
 popd
