e6e7cec61a11283537b8231074452bde7e54ddce
Peter Gadfort <peter.gadfort@gmail.com>
2025-05-20 20:32:13 -0400

    fix we_in references

--- lambdapdk/asap7/libs/fakeram7/build.sh
+++ lambdapdk/asap7/libs/fakeram7/build.sh
@@ -13,7 +13,7 @@ mkdir -p $src_path/lef $src_path/nldm $src_path/verilog
 
 git clone https://github.com/gadfort/FakeRAM2.0.git
 pushd FakeRAM2.0
-git checkout cb17ef7a2af3328c445ed60bcb39a034dec9e261
+git checkout 82877a8eaf9c09d9034e90bb49521603adfda358
 
 python3 run.py $src_path/fakeram7.cfg --output_dir $work_dir/results
 
