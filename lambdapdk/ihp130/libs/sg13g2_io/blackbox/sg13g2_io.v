647dc2da9c081227aeabb538d71389f2a69dd555
Peter Gadfort <peter.gadfort@gmail.com>
2025-11-19 11:02:57 -0500

    fix pin directions in blackbox models

--- lambdapdk/ihp130/libs/sg13g2_io/blackbox/sg13g2_io.v
+++ lambdapdk/ihp130/libs/sg13g2_io/blackbox/sg13g2_io.v
@@ -1,4 +1,4 @@
-// Source ../.sc/cache/ihp130-68eebafcd9b2f5e92c69d37a8d3d90eb266550f5/ihp-sg13g2/libs.ref/sg13g2_io/lef/sg13g2_io.lef
+// Source ../.sc/cache/ihp130-62c1d640dc1c91f5-1d3cde26ed72789c/ihp-sg13g2/libs.ref/sg13g2_io/lef/sg13g2_io.lef
 
 (* blackbox *)
 module sg13g2_Corner (
@@ -67,7 +67,7 @@ endmodule
 module sg13g2_IOPadIn (
     inout iovdd,
     inout iovss,
-    inout p2c,
+    output p2c,
     inout pad,
     inout vdd,
     inout vss
@@ -76,7 +76,7 @@ endmodule
 
 (* blackbox *)
 module sg13g2_IOPadOut4mA (
-    inout c2p,
+    input c2p,
     inout iovdd,
     inout iovss,
     inout pad,
@@ -87,7 +87,7 @@ endmodule
 
 (* blackbox *)
 module sg13g2_IOPadOut16mA (
-    inout c2p,
+    input c2p,
     inout iovdd,
     inout iovss,
     inout pad,
@@ -98,7 +98,7 @@ endmodule
 
 (* blackbox *)
 module sg13g2_IOPadOut30mA (
-    inout c2p,
+    input c2p,
     inout iovdd,
     inout iovss,
     inout pad,
@@ -109,8 +109,8 @@ endmodule
 
 (* blackbox *)
 module sg13g2_IOPadTriOut4mA (
-    inout c2p,
-    inout c2p_en,
+    input c2p,
+    input c2p_en,
     inout iovdd,
     inout iovss,
     inout pad,
@@ -121,8 +121,8 @@ endmodule
 
 (* blackbox *)
 module sg13g2_IOPadTriOut16mA (
-    inout c2p,
-    inout c2p_en,
+    input c2p,
+    input c2p_en,
     inout iovdd,
     inout iovss,
     inout pad,
@@ -133,8 +133,8 @@ endmodule
 
 (* blackbox *)
 module sg13g2_IOPadTriOut30mA (
-    inout c2p,
-    inout c2p_en,
+    input c2p,
+    input c2p_en,
     inout iovdd,
     inout iovss,
     inout pad,
@@ -145,11 +145,11 @@ endmodule
 
 (* blackbox *)
 module sg13g2_IOPadInOut4mA (
-    inout c2p,
-    inout c2p_en,
+    input c2p,
+    input c2p_en,
     inout iovdd,
     inout iovss,
-    inout p2c,
+    output p2c,
     inout pad,
     inout vdd,
     inout vss
@@ -158,11 +158,11 @@ endmodule
 
 (* blackbox *)
 module sg13g2_IOPadInOut16mA (
-    inout c2p,
-    inout c2p_en,
+    input c2p,
+    input c2p_en,
     inout iovdd,
     inout iovss,
-    inout p2c,
+    output p2c,
     inout pad,
     inout vdd,
     inout vss
@@ -171,11 +171,11 @@ endmodule
 
 (* blackbox *)
 module sg13g2_IOPadInOut30mA (
-    inout c2p,
-    inout c2p_en,
+    input c2p,
+    input c2p_en,
     inout iovdd,
     inout iovss,
-    inout p2c,
+    output p2c,
     inout pad,
     inout vdd,
     inout vss
